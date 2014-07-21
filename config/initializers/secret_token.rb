018e3230c7c4d5d7502da4a87f5fed83a6706c41
hzlbrc <bagburcu@hotmail.com>
2014-07-22 00:55:26 +0300

    Uygulama iskeleti oluşturuldu.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GorevYoneticisi::Application.config.secret_key_base = '76d62af11db294c11cc33699fd71596ad1bc19669a996e02889a1d33769824a55917d93aa928753c48a8daadb5da7830cdfd90607a7eb0c652d10531815f3e1b'
